de605c0f67074a93e51ac2cda69866df4abe224b
minsiechung <minsiechung@gmail.com>
2023-11-29 02:00:44 +0900

    fix

--- src/components/part/portfolio/Portfolio.tsx
+++ src/components/part/portfolio/Portfolio.tsx
@@ -6,7 +6,6 @@ import { HoverDefaultstyle } from '@/style/common.style';
 import { mobileMedia } from '@/style/deviceWidth';
 import { ReactElement, useEffect, useRef, useState } from 'react';
 import { css, styled } from 'styled-components';
-import PortfolioModal from './modal/PortfolioModal';
 
 const Portfolio = (): ReactElement => {
   const pagePosition = usePagePositionSelector();
@@ -73,7 +72,8 @@ const Portfolio = (): ReactElement => {
         ))}
       </DIV_ContentSection>
       {portfolioModal?.kind === 'portfolio' && (
-        <PortfolioModal onClose={() => setPortfolioModal(undefined)} portfolio={portfolioModal} />
+        <></>
+        // <PortfolioModal onClose={() => setPortfolioModal(undefined)} portfolio={portfolioModal} />
       )}
     </DIV_PortfolioWrap>
   );
